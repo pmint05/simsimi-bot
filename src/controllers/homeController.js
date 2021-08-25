f9605d6577c9e445b76dcfaa45c7a47c3b952407
Pham Thong <phucon547@gmail.com>
2021-08-25 19:55:09 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,34 +4,40 @@ import chatbotServices from "../services/chatbotServices";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-async function callSendAPI(sender_psid, response) {
-	// Construct the message body
-	let request_body = {
-		recipient: {
-			id: sender_psid,
-		},
-		message: response,
-	};
-	await chatbotServices.sendTypingOn(sender_psid);
-	await chatbotServices.sendMarkReadMessage(sender_psid);
+let callSendAPI = async (sender_psid, response) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			// Construct the message body
+			let request_body = {
+				recipient: {
+					id: sender_psid,
+				},
+				message: response,
+			};
+			await chatbotServices.sendTypingOn(sender_psid);
+			await chatbotServices.sendMarkReadMessage(sender_psid);
 
-	// Send the HTTP request to the Messenger Platform
-	request(
-		{
-			uri: "https://graph.facebook.com/v2.6/me/messages",
-			qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
-			method: "POST",
-			json: request_body,
-		},
-		(err, res, body) => {
-			if (!err) {
-				console.log("message sent!");
-			} else {
-				console.error("Unable to send message:" + err);
-			}
+			// Send the HTTP request to the Messenger Platform
+			request(
+				{
+					uri: "https://graph.facebook.com/v2.6/me/messages",
+					qs: { access_token: PAGE_ACCESS_TOKEN },
+					method: "POST",
+					json: request_body,
+				},
+				(err, res, body) => {
+					if (!err) {
+						resolve("message sent!");
+					} else {
+						console.error("Unable to send message:" + err);
+					}
+				}
+			);
+		} catch (error) {
+			reject(error);
 		}
-	);
-}
+	});
+};
 let getHomePage = (req, res) => {
 	return res.render("homepage.ejs");
 };
