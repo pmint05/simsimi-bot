07f8ecf9f723b70dbe69b5e608db8f398e297de5
Pham Thong <phucon547@gmail.com>
2021-08-25 19:58:01 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -136,7 +136,7 @@ async function handleMessage(sender_psid, received_message) {
 	}
 
 	// Send the response message
-	callSendAPI(sender_psid, response);
+	await callSendAPI(sender_psid, response);
 }
 
 // Handles messaging_postbacks events
